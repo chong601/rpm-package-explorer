d839e1a50daa4229fe47020574f5d737d434877b
Chong Jin Yi <chong601.13@gmail.com>
2021-07-14 20:09:41 +0800

    Add more parser

--- rpm_package_explorer/xmlparser.py
+++ rpm_package_explorer/xmlparser.py
@@ -1,24 +1,24 @@
-import declxml as xml
+import declxml as dxml
 import json
-from typing import Union
+from typing import NamedTuple, Union
 from io import BytesIO, FileIO
 
 
 def parse_repomd(filename: str):
-    data_processor = xml.array(xml.dictionary('data', [
-        xml.string('.', attribute='type'),
-        xml.string('checksum', 'type', alias='checksum_hash_type'),
-        xml.string('checksum', alias='checksum_hash'),
-        xml.string('open-checksum', 'type', alias='open_checksum_hash_type', default=None, required=False),
-        xml.string('open-checksum', alias='open_checksum_hash', default=None, required=False),
-        xml.string('location', attribute='href'),
-        xml.integer('timestamp'),
-        xml.integer('size'),
-        xml.integer('open-size', default=None, alias='open_size', required=False),
-        xml.integer('database_version', default=None, required=False)
+    data_processor = dxml.array(dxml.dictionary('data', [
+        dxml.string('.', attribute='type'),
+        dxml.string('checksum', 'type', alias='checksum_hash_type'),
+        dxml.string('checksum', alias='checksum_hash'),
+        dxml.string('open-checksum', 'type', alias='open_checksum_hash_type', default=None, required=False),
+        dxml.string('open-checksum', alias='open_checksum_hash', default=None, required=False),
+        dxml.string('location', attribute='href'),
+        dxml.integer('timestamp'),
+        dxml.integer('size'),
+        dxml.integer('open-size', default=None, alias='open_size', required=False),
+        dxml.integer('database_version', default=None, required=False)
     ]))
-    repomd_processor = xml.dictionary('repomd', [data_processor])
-    return rearrange_data(xml.parse_from_file(repomd_processor, filename), 'type')
+    repomd_processor = dxml.dictionary('repomd', [data_processor])
+    return rearrange_data(dxml.parse_from_file(repomd_processor, filename), 'type')
 
 
 def rearrange_data(xml_dict: dict, key_name):
@@ -34,20 +34,90 @@ def rearrange_data(xml_dict: dict, key_name):
 
     return new_dict
 
-def parse_filelists():
-    package_processor = xml.array(xml.dictionary(
+
+def parse_filelists(filename: str):
+    package_processor = dxml.array(dxml.dictionary(
         'package', [
-            xml.string('.', 'pkgid'),
-            xml.string('.', 'name'),
-            xml.string('.', 'arch'),
-            xml.integer('version', 'epoch'),
-            xml.string('version', 'ver', alias='version'),
-            xml.string('version', 'rel', alias='release'),
-            xml.array(
-                xml.dictionary('file', [
-                    xml.string('.', 'type', default='file', required=False, omit_empty=True),
-                    xml.string('.', alias='name')
-                ]))
+            dxml.string('.', 'pkgid'),
+            dxml.string('.', 'name'),
+            dxml.string('.', 'arch'),
+            dxml.integer('version', 'epoch'),
+            dxml.string('version', 'ver', alias='version'),
+            dxml.string('version', 'rel', alias='release'),
+            dxml.array(
+                dxml.dictionary('file', [
+                    dxml.string('.', 'type', default='file', required=False),
+                    dxml.string('.', alias='name')
+                ], required=False))
         ]))
-    filelists_processor = xml.dictionary('filelists', [package_processor])
-    return rearrange_data(xml.parse_from_string(filelists_processor, xml_data), key_name='pkgid')
+    filelists_processor = dxml.dictionary('filelists', [package_processor])
+    return rearrange_data(dxml.parse_from_file(filelists_processor, filename), key_name='pkgid')
+
+
+def parse_otherdata(filename: str):
+    package_processor = dxml.array(dxml.dictionary(
+        'package', [
+            dxml.string('.', 'pkgid'),
+            dxml.string('.', 'name'),
+            dxml.string('.', 'arch'),
+            dxml.integer('version', 'epoch'),
+            dxml.string('version', 'ver', alias='version'),
+            dxml.string('version', 'rel', alias='release'),
+            dxml.array(
+                dxml.dictionary('changelog', [
+                    dxml.string('.', 'author'),
+                    dxml.integer('.', 'date'),
+                    dxml.string('.', alias='changelog')
+                ], required=False))
+        ]))
+    otherlist_processor = dxml.dictionary('otherdata', [package_processor])
+    return rearrange_data(dxml.parse_from_file(otherlist_processor, filename), key_name='pkgid')
+
+
+def parse_updateinfo(filename: str):
+    reference_processor = dxml.dictionary('reference', [
+        dxml.string('.', 'href'),
+        dxml.string('.', 'id'),
+        dxml.string('.', 'type'),
+        dxml.string('.', 'title')
+    ], required=False)
+
+    package_processor = dxml.dictionary('package', [
+        dxml.string('.', 'name', alias='name'),
+        dxml.string('.', 'version', alias='version'),
+        dxml.string('.', 'release', alias='release'),
+        dxml.integer('.', 'epoch', alias='epoch'),
+        dxml.string('.', 'arch', alias='arch'),
+        dxml.string('.', 'src', alias='src'),
+        dxml.string('filename'),
+        dxml.string('sum', 'type', alias='hash_type'),
+        dxml.string('sum', alias='hash')
+    ])
+
+    collection_processor = dxml.dictionary('pkglist/collection', [
+        dxml.string('.', 'short', alias='collection_shortname'),
+        dxml.string('name'),
+        dxml.array(package_processor)
+    ])
+
+    update_processor = dxml.array(dxml.dictionary(
+        'update', [
+            dxml.string('.', 'from'),
+            dxml.string('.', 'status'),
+            dxml.string('.', 'type'),
+            dxml.integer('.', 'version'),
+            dxml.string('id'),
+            dxml.string('title'),
+            dxml.string('issued', 'date', alias='issued_date'),
+            dxml.string('updated', 'date', alias='updated_date'),
+            dxml.string('rights'),
+            dxml.integer('release'),
+            dxml.integer('pushcount'),
+            dxml.string('severity'),
+            dxml.string('summary'),
+            dxml.string('description'),
+            dxml.array(reference_processor, nested='references'),
+            dxml.array(collection_processor, alias='collection')
+        ]))
+    updates_processor = dxml.dictionary('updates', [update_processor])
+    return rearrange_data(dxml.parse_from_file(updates_processor, filename), key_name='id')
