7041efdee5568ef7abe6344d228c4009e555b2a3
Chong Jin Yi <chong601.13@gmail.com>
2021-07-15 00:17:34 +0800

    Clean up xmlparser.py - Add missed out primary.xml parser - Remove unused imports - Do not rearrange data (let the calling code to do that instead)

--- rpm_package_explorer/xmlparser.py
+++ rpm_package_explorer/xmlparser.py
@@ -1,7 +1,4 @@
 import declxml as dxml
-import json
-from typing import NamedTuple, Union
-from io import BytesIO, FileIO
 
 
 def parse_repomd(filename: str):
@@ -51,7 +48,7 @@ def parse_filelists(filename: str):
                 ], required=False))
         ]))
     filelists_processor = dxml.dictionary('filelists', [package_processor])
-    return rearrange_data(dxml.parse_from_file(filelists_processor, filename), key_name='pkgid')
+    return dxml.parse_from_file(filelists_processor, filename)
 
 
 def parse_otherdata(filename: str):
@@ -71,7 +68,7 @@ def parse_otherdata(filename: str):
                 ], required=False))
         ]))
     otherlist_processor = dxml.dictionary('otherdata', [package_processor])
-    return rearrange_data(dxml.parse_from_file(otherlist_processor, filename), key_name='pkgid')
+    return dxml.parse_from_file(otherlist_processor, filename)
 
 
 def parse_updateinfo(filename: str):
@@ -120,4 +117,92 @@ def parse_updateinfo(filename: str):
             dxml.array(collection_processor, alias='collection')
         ]))
     updates_processor = dxml.dictionary('updates', [update_processor])
-    return rearrange_data(dxml.parse_from_file(updates_processor, filename), key_name='id')
+    return dxml.parse_from_file(updates_processor, filename)
+
+
+def parse_primary(filename: str):
+    version_processor = dxml.dictionary('version', [
+        dxml.integer('.', 'epoch', alias='epoch'),
+        dxml.string('.', 'ver', alias='ver'),
+        dxml.string('.', 'rel', alias='rel')
+    ])
+
+    checksum_processor = dxml.dictionary('checksum', [
+        dxml.string('.', 'type', alias='type'),
+        dxml.string('.', alias='pkgid')
+    ])
+
+    time_processor = dxml.dictionary('time', [
+        dxml.integer('.', 'file', 'file'),
+        dxml.integer('.', 'build')
+    ])
+
+    size_processor = dxml.dictionary('size', [
+        dxml.integer('.', 'package'),
+        dxml.integer('.', 'installed'),
+        dxml.integer('.', 'archive')
+    ])
+
+    location_processor = dxml.dictionary('location', [
+        dxml.string('.', 'href')
+    ])
+
+    header_range_processor = dxml.dictionary('header-range', [
+        dxml.integer('.', 'start'),
+        dxml.integer('.', 'end')
+    ])
+
+    provides_processor = dxml.array(dxml.dictionary('entry', [
+        dxml.string('.', 'name'),
+        dxml.string('.', 'flags', required=False, default=None),
+        dxml.integer('.', 'epoch', required=False, default=None),
+        dxml.string('.', 'ver', required=False, default=None),
+        dxml.string('.', 'rel', required=False, default=None)
+    ], required=False), nested='provides')
+
+    requires_processor = dxml.array(dxml.dictionary('entry', [
+        dxml.string('.', 'name'),
+        dxml.string('.', 'flags', required=False, default=None),
+        dxml.integer('.', 'epoch', required=False, default=None),
+        dxml.string('.', 'ver', required=False, default=None),
+        dxml.string('.', 'rel', required=False, default=None),
+        dxml.integer('.', 'pre', required=False, default=None)
+    ], required=False), nested='requires')
+
+    conflicts_processor = dxml.array(dxml.dictionary('entry', [
+        dxml.string('.', 'name'),
+        dxml.string('.', 'flags', required=False, default=None),
+        dxml.integer('.', 'epoch', required=False, default=None),
+        dxml.string('.', 'ver', required=False, default=None),
+        dxml.string('.', 'rel', required=False, default=None)
+    ], required=False), nested='conflicts')
+
+    format_processor = dxml.dictionary('format', [
+        dxml.string('license'),
+        dxml.string('vendor'),
+        dxml.string('group'),
+        dxml.string('buildhost'),
+        dxml.string('sourcerpm'),
+        header_range_processor,
+        provides_processor,
+        requires_processor,
+        conflicts_processor
+    ])
+
+    package_processor = dxml.array(dxml.dictionary('package', [
+        dxml.string('name'),
+        dxml.string('arch'),
+        version_processor,
+        checksum_processor,
+        dxml.string('summary'),
+        dxml.string('description'),
+        dxml.string('packager'),
+        dxml.string('url'),
+        time_processor,
+        size_processor,
+        location_processor,
+        format_processor
+    ]))
+
+    metadata_processor = dxml.dictionary('metadata', [package_processor])
+    return dxml.parse_from_file(metadata_processor, filename)
