3e61677f273f664a4d6746aa27b697e7bf81c9ed
Chong Jin Yi <chong601.13@gmail.com>
2021-07-18 07:37:11 +0800

    Modify exceptions.py - Add more database exceptions - Add docstrings

--- rpm_package_explorer/exceptions.py
+++ rpm_package_explorer/exceptions.py
@@ -1,3 +1,5 @@
+# TODO: add/import version support here
+
 class InvalidState(Exception):
     """Used when repomd data reaches an unexpected state"""
     pass
@@ -6,6 +8,19 @@ class InvalidState(Exception):
 class UnsupportedFileListException(Exception):
     """Used when the file list version is unsupported"""
     def __init__(self, version):
-        super().__init__(f'This file list version is unsupported. Please raise an issue. '
+        super().__init__(f'This file list database version is unsupported. Please raise an issue. '
                          f'Currently support version {version} only.')
 
+
+class UnsupportedPrimaryDatabaseException(Exception):
+    """Used when the primary version is unsupported"""
+    def __init__(self, version) -> None:
+        super().__init__(f'This primary database version is unsupported. Please raise an issue. '
+                         f'Currently support version {version} only.')
+
+
+class UnsupportedOtherDatabaseException(Exception):
+    """Used when the other version is unsupported"""
+    def __init__(self, version) -> None:
+        super().__init__(f'This other database version is unsupported. Please raise an issue. '
+                         f'Currently support version {version} only.')
